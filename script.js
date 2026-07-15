caf0ac46cc6b7ec91e0e748f49929cfa82549f03
jagheterjohans <johanaxelnyman@gmail.com>
2026-07-15 10:29:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-console.log("Vattenkoll startad!");
-
-fetch("kommuner.json")
+fetch("data/kommuner.json")
   .then(response => response.json())
-  .then(data => {
-    console.log(data);
+  .then(kommuner => {
+
+    const lista = document.getElementById("kommunlista");
+
+    kommuner.forEach(kommun => {
+
+      const div = document.createElement("div");
+
+      div.innerHTML = `
+        <h2>${kommun.namn}</h2>
+        <button onclick="visaKommun('${kommun.id}')">
+          Visa vattenprofil
+        </button>
+      `;
+
+      lista.appendChild(div);
+
+    });
+
   });
+
+function visaKommun(id){
+
+    fetch(`data/${id}.json`)
+      .then(response => response.json())
+      .then(data => {
+
+          alert(
+            `${data.kommun}\n\nKaffe: ${data.kaffe}/10\nSmak: ${data.smak}/10`
+          );
+
+      });
+
+}
+Nu händer följande:
